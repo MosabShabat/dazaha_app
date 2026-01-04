1956e0709900d94b66f671b6f0f9149e54c3df86
MosabShabat <mosabshabat39@gmail.com>
2026-01-04 10:55:34 +0200

    تحديث الملفات الأخيرة

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,8 +13,8 @@ val localProperties = Properties().apply {
     val localPropertiesFile = rootProject.file("local.properties")
     if (localPropertiesFile.exists()) load(FileInputStream(localPropertiesFile))
 }
-val flutterVersionCode = localProperties.getProperty("flutter.versionCode")?.toInt() ?: 32
-val flutterVersionName = localProperties.getProperty("flutter.versionName") ?: "1.0.32"
+val flutterVersionCode = localProperties.getProperty("flutter.versionCode")?.toInt() ?: 33
+val flutterVersionName = localProperties.getProperty("flutter.versionName") ?: "1.0.33"
 
 // قراءة keystore
 val keystoreProperties = Properties()
