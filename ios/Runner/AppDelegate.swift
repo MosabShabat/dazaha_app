a0e6b03c11b0283d5d711d9e86a63f43919c684a
Majed Hussein <majed.f.hussein98@gmail.com>
2026-01-14 10:14:01 +0200

    Fixed isuess  ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,4 +1,6 @@
+import Firebase
 import Flutter
+import GoogleMaps
 import UIKit
 
 @main
@@ -7,7 +9,16 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyBpGO8PYXUHsGCPp6_Vo7XsJROQFqTnv2w")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  override func userNotificationCenter(
+    _ center: UNUserNotificationCenter,
+    willPresent notification: UNNotification,
+    withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
+  ) {
+    completionHandler([.banner, .sound, .badge])
   }
 }
